9b47275197d870f9b722501670e4d5b5e7be85ad
lucas.marques <lucasmarquesdevti@gmail.com>
2023-07-13 00:50:37 -0300

    Update-Delete-criados

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -8,5 +8,20 @@ namespace Controle_de_contatos.Controllers
         {
             return View();
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
     }
 }
